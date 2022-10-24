a26d46c01c498f72e2489ce261c5eaf69b3b5e90
Gabriel Cortez <96384061+05041994@users.noreply.github.com>
2022-10-24 16:00:42 -0300

    Add files via upload
    hello

---
+++ Lista04/Abastecimento de Combustível/Abastecimento.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abastecimento_de_Combustível
+{
+    class Abastecimento
+    {
+        private double ValorLitro;
+        private double ValorPago;
+        private double ValorKmrodado;
+
+        public void SetValorLitro(double ValorLitro)
+        {
+            if (ValorLitro > 0) this.ValorLitro = ValorLitro;
+        }
+        public void SetValorPago(double ValorPago)
+        {
+            if (ValorPago > 0) this.ValorPago = ValorPago;
+        }
+        public void SetValorKmrodado(double ValorKmrodado)
+        {
+            if (ValorKmrodado > 0) this.ValorKmrodado = ValorKmrodado;
+        }
+        public double MediaKmlitro()
+        {
+            double numLitro = ValorPago / ValorLitro;
+            return ValorKmrodado / numLitro;
+        }
+        public double MediaReaiskm()
+        {
+            return ValorPago / ValorKmrodado;
+        }
+    }
+}
---
+++ Lista04/Abastecimento de Combustível/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Abastecimento_de_Combustível
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Abastecimento obj = new Abastecimento();
+            obj.SetValorKmrodado(double.Parse(ValorKmrodado.Text));
+            obj.SetValorPago(double.Parse(Valogasto.Text));
+            obj.SetValorLitro(double.Parse(ValorLitro.Text));
+            kmlitro.Text = obj.MediaKmlitro().ToString("0.00");
+            Reais.Text = obj.MediaReaiskm().ToString("0.00");
+
+        }
+    }
+}
